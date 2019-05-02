dd6d5b8a1cc1ba93c6c72c87014fdfa19a7f9315
Seva Safris <seva@safris.org>
2019-05-02 23:22:34 +0200

    Rebrand as OpenJAX

---
+++ src/main/java/org/openjax/sql/AuditConnection.java
@@ -0,0 +1,204 @@
+/* Copyright (c) 2009 OpenJAX
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of The MIT License (MIT) along with this
+ * program. If not, see <http://opensource.org/licenses/MIT/>.
+ */
+
+package org.openjax.sql;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A {@link Connection} that delegates all method calls to another connection.
+ * The sole purpose of this class is to override all "create" and "prepare"
+ * methods to return "Audit" implementations of the respective return types.
+ *
+ * @see AuditStatement
+ * @see AuditPreparedStatement
+ * @see AuditCallableStatement
+ */
+public class AuditConnection extends DelegateConnection {
+  private static final Logger logger = LoggerFactory.getLogger(AuditConnection.class);
+
+  /**
+   * Releases the specified {@link Connection} object's database and JDBC
+   * resources immediately instead of waiting for them to be automatically
+   * released.
+   * <p>
+   * This method differs itself from {@link Connection#close()} by not throwing
+   * a {@link SQLException} if a database access error occurs. If a database
+   * access error occurs, a warning will be logged to the logger associated with
+   * the {@code AuditConnection} class.
+   *
+   * @param connection The connection to be closed.
+   * @throws NullPointerException If {@code connection} is null.
+   */
+  public static void close(final Connection connection) {
+    try {
+      if (!connection.isClosed())
+        connection.close();
+    }
+    catch (final SQLException e) {
+      logger.warn(e.getMessage(), e);
+    }
+  }
+
+  /**
+   * Creates a new {@code AuditConnection} with the specified {@code target} to
+   * which all method calls will be delegated.
+   *
+   * @param target The {@link Connection} to which all method calls will be
+   *          delegated.
+   */
+  public AuditConnection(final Connection target) {
+    super(target);
+  }
+
+  /**
+   * {@inheritDoc}
+   * <p>
+   * <i><b>Note:</b> This method returns an {@link AuditStatement} instance that
+   * delegates all method calls to the underlying {@link Statement}.</i>
+   */
+  @Override
+  public Statement createStatement() throws SQLException {
+    return new AuditStatement(target.createStatement());
+  }
+
+  /**
+   * {@inheritDoc}
+   * <p>
+   * <i><b>Note:</b> This method returns an {@link AuditPreparedStatement}
+   * instance that delegates all method calls to the underlying
+   * {@link PreparedStatement}.</i>
+   */
+  @Override
+  public PreparedStatement prepareStatement(final String sql) throws SQLException {
+    return new AuditPreparedStatement(target.prepareStatement(sql), sql);
+  }
+
+  /**
+   * {@inheritDoc}
+   * <p>
+   * <i><b>Note:</b> This method returns an {@link AuditStatement}
+   * instance that delegates all method calls to the underlying
+   * {@link Statement}.</i>
+   */
+  @Override
+  public Statement createStatement(final int resultSetType, final int resultSetConcurrency) throws SQLException {
+    return new AuditStatement(target.createStatement(resultSetType, resultSetConcurrency));
+  }
+
+  /**
+   * {@inheritDoc}
+   * <p>
+   * <i><b>Note:</b> This method returns an {@link AuditPreparedStatement}
+   * instance that delegates all method calls to the underlying
+   * {@link PreparedStatement}.</i>
+   */
+  @Override
+  public PreparedStatement prepareStatement(final String sql, final int resultSetType, final int resultSetConcurrency) throws SQLException {
+    return new AuditPreparedStatement(target.prepareStatement(sql, resultSetType, resultSetConcurrency), sql);
+  }
+
+  /**
+   * {@inheritDoc}
+   * <p>
+   * <i><b>Note:</b> This method returns an {@link AuditCallableStatement}
+   * instance that delegates all method calls to the underlying
+   * {@link CallableStatement}.</i>
+   */
+  @Override
+  public CallableStatement prepareCall(final String sql, final int resultSetType, final int resultSetConcurrency) throws SQLException {
+    return new AuditCallableStatement(target.prepareCall(sql, resultSetType, resultSetConcurrency), sql);
+  }
+
+  /**
+   * {@inheritDoc}
+   * <p>
+   * <i><b>Note:</b> This method returns an {@link AuditStatement}
+   * instance that delegates all method calls to the underlying
+   * {@link Statement}.</i>
+   */
+  @Override
+  public Statement createStatement(final int resultSetType, final int resultSetConcurrency, final int resultSetHoldability) throws SQLException {
+    return new AuditStatement(target.createStatement(resultSetType, resultSetConcurrency, resultSetHoldability));
+  }
+
+  /**
+   * {@inheritDoc}
+   * <p>
+   * <i><b>Note:</b> This method returns an {@link AuditPreparedStatement}
+   * instance that delegates all method calls to the underlying
+   * {@link PreparedStatement}.</i>
+   */
+  @Override
+  public PreparedStatement prepareStatement(final String sql, final int resultSetType, int resultSetConcurrency, final int resultSetHoldability) throws SQLException {
+    return new AuditPreparedStatement(target.prepareStatement(sql, resultSetType, resultSetConcurrency, resultSetHoldability), sql);
+  }
+
+  /**
+   * {@inheritDoc}
+   * <p>
+   * <i><b>Note:</b> This method returns an {@link AuditCallableStatement}
+   * instance that delegates all method calls to the underlying
+   * {@link CallableStatement}.</i>
+   */
+  @Override
+  public CallableStatement prepareCall(final String sql, final int resultSetType, int resultSetConcurrency, final int resultSetHoldability) throws SQLException {
+    return new AuditCallableStatement(target.prepareCall(sql, resultSetType, resultSetConcurrency, resultSetHoldability), sql);
+  }
+
+  /**
+   * {@inheritDoc}
+   * <p>
+   * <i><b>Note:</b> This method returns an {@link AuditPreparedStatement}
+   * instance that delegates all method calls to the underlying
+   * {@link PreparedStatement}.</i>
+   */
+  @Override
+  public PreparedStatement prepareStatement(final String sql, final int autoGeneratedKeys) throws SQLException {
+    return new AuditPreparedStatement(target.prepareStatement(sql, autoGeneratedKeys), sql);
+  }
+
+  /**
+   * {@inheritDoc}
+   * <p>
+   * <i><b>Note:</b> This method returns an {@link AuditPreparedStatement}
+   * instance that delegates all method calls to the underlying
+   * {@link PreparedStatement}.</i>
+   */
+  @Override
+  public PreparedStatement prepareStatement(final String sql, final int[] columnIndexes) throws SQLException {
+    return new AuditPreparedStatement(target.prepareStatement(sql, columnIndexes), sql);
+  }
+
+  /**
+   * {@inheritDoc}
+   * <p>
+   * <i><b>Note:</b> This method returns an {@link AuditPreparedStatement}
+   * instance that delegates all method calls to the underlying
+   * {@link PreparedStatement}.</i>
+   */
+  @Override
+  public PreparedStatement prepareStatement(final String sql, final String[] columnNames) throws SQLException {
+    return new AuditPreparedStatement(target.prepareStatement(sql, columnNames), sql);
+  }
+}
---
+++ src/test/java/org/openjax/sql/SQLFormatTest.java
@@ -0,0 +1,31 @@
+/* Copyright (c) 2017 OpenJAX
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of The MIT License (MIT) along with this
+ * program. If not, see <http://opensource.org/licenses/MIT/>.
+ */
+
+package org.openjax.sql;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SQLFormatTest {
+  private static final Logger logger = LoggerFactory.getLogger(SQLFormatTest.class);
+
+  @Test
+  public void testFormat() {
+    final String sql = "SELECT a.address1, a.address2, a.city, a.locality, a.postal_code, a.country, a.latitude, a.longitude, a.customer_number, a.company_name, a.first_name, a.last_name, a.phone, a.sales_employee_number, a.credit_limit, 7918 * ATAN2(SQRT(EXP(LN(SIN(a.latitude - 37.78536811469731 * PI() / 360)) * 2) + COS(a.latitude * PI() / 360) * EXP(LN(SIN(a.longitude - -122.3931884765625 * PI() / 360)) * 2)), SQRT(EXP(LN(1 - SIN(a.latitude - 37.78536811469731 * PI() / 360)) * 2) * COS(PI() * 37.78536811469731 / 180) * COS(a.latitude * PI() / 180) * EXP(LN(SIN(a.longitude - -122.3931884765625 * PI() / 360)) * 2))) AS c FROM customer a GROUP BY a.address1, a.address2, a.city, a.locality, a.postal_code, a.country, a.latitude, a.longitude, a.customer_number, a.company_name, a.first_name, a.last_name, a.phone, a.sales_employee_number, a.credit_limit HAVING 7918 * ATAN2(SQRT(EXP(LN(SIN(a.latitude - 37.78536811469731 * PI() / 360)) * 2) + COS(a.latitude * PI() / 360) * EXP(LN(SIN(a.longitude - -122.3931884765625 * PI() / 360)) * 2)), SQRT(EXP(LN(1 - SIN(a.latitude - 37.78536811469731 * PI() / 360)) * 2) * COS(PI() * 37.78536811469731 / 180) * COS(a.latitude * PI() / 180) * EXP(LN(SIN(a.longitude - -122.3931884765625 * PI() / 360)) * 2))) < 10 AND 7918 * ATAN2(SQRT(EXP(LN(SIN(a.latitude - 37.78536811469731 * PI() / 360)) * 2) + COS(a.latitude * PI() / 360) * EXP(LN(SIN(a.longitude - -122.3931884765625 * PI() / 360)) * 2)), SQRT(EXP(LN(1 - SIN(a.latitude - 37.78536811469731 * PI() / 360)) * 2) * COS(PI() * 37.78536811469731 / 180) * COS(a.latitude * PI() / 180) * EXP(LN(SIN(a.longitude - -122.3931884765625 * PI() / 360)) * 2))) < 10 ORDER BY c DESC FETCH FIRST 10 ROWS ONLY";
+    logger.info(SQLFormat.format(sql));
+  }
+}
